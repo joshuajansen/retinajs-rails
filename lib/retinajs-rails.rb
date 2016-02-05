76913640aa18b2e519d628c142b68ae4f2b954df
Joshua Jansen <joshuajansen88@gmail.com>
2016-02-05 12:27:18 +0100

    Move ImageHelper to retinajs-rails

--- lib/retinajs-rails.rb
+++ lib/retinajs-rails.rb
@@ -1,7 +1,12 @@
-require "image_helper"
-ActionView::Base.send :include, ImageHelper
-
 module RetinaJs
   class Engine < ::Rails::Engine
   end
 end
+
+module ImageHelper
+  def image_tag_with_at2x(name_at_1x, options={})
+    name_at_2x = name_at_1x.gsub(%r{\.\w+$}, '@2x\0')
+    image_tag(name_at_1x, options.merge("data-at2x" => asset_path(name_at_2x)))
+  end
+end
+ActionView::Base.send :include, ImageHelper
